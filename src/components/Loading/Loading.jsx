ec18fc0f81c8b123764d31d3446c037dbd4adfca
Brian McPhillips <B.McPhillips89@gmail.com>
2021-12-20 16:07:35 -0800

    Loading component and test

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loading = () => (
+  <img 
+    src="https://media0.giphy.com/media/VJCSpN9VaG7K4p3CUj/source.gif" 
+    alt="Loading" 
+  />
+);
+
+export default Loading;
--- src/containers/AllVillagers/AllVillagers.jsx
+++ src/containers/AllVillagers/AllVillagers.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 
 export default class AllVillagers extends Component {
+  state = {
+    villagers: [],
+    loading: true,
+    error: false
+  }
+
+  componentDidMount = () => {
+    fetch('villater.com')
+      .then(res => res.json())
+      .then(villagers => this.setState({ villagers }));
+  }
+
+
   render() {
+    const { villagers, loading, error } = this.state;
     return (
       <div>
         
